b2b7fdefb6eb9f61546584a06101d83c8eca9cd0
András Guseo <andras@divi-magazine.com>
2021-01-07 18:07:54 +0100

    Updated download URL

--- liveagent/liveagent-plugin-versions.user.js
+++ liveagent/liveagent-plugin-versions.user.js
@@ -6,7 +6,7 @@
 // @author       Andras Guseo
 // @include      https://theeventscalendar.ladesk.com/agent/*
 // @include      https://support.theeventscalendar.com/agent/*
-// @downloadURL  https://github.com/moderntribe/tampermonkey-scripts/raw/master/liveagent/liveagent-plugin-versions.user.js
+// @downloadURL  https://github.com/the-events-calendar/tampermonkey-scripts/raw/main/liveagent/liveagent-plugin-versions.user.js
 // @grant        none
 // ==/UserScript==
 
