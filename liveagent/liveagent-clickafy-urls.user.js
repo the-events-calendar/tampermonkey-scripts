c0155b6125137c424397ee95c793cd59ec4e0a9e
András Guseo <andras@divi-magazine.com>
2021-03-05 12:11:51 +0100

    Code cleanup #2

--- liveagent/liveagent-clickafy-urls.user.js
+++ liveagent/liveagent-clickafy-urls.user.js
@@ -11,105 +11,105 @@
 // ==/UserScript==
 
 (function() {
-	'use strict';
+		'use strict';
 
-	// If you set this to true you will see log messages in the console
-	var log = false;
+		// If you set this to true you will see log messages in the console
+		var log = false;
 
-	console.log( 'Starting Clickafy Script' );
+		console.log( 'Starting Clickafy Script' );
 
-	// Run the script every 5 seconds. This is necessary due to the dynamic nature of LiveAgent
-	var startScript = window.setInterval( clickableScript, 5000 );
+		// Run the script every 5 seconds. This is necessary due to the dynamic nature of LiveAgent
+		var startScript = window.setInterval( clickableScript, 5000 );
 
-	// Variables
-	var fields = [ "Central ID", "Issue Tracker ID", "Site's URL", "WordPress ID", "Sandbox URL", "url", "braintree_url" ];
-	var field = "",
-		url = "",
-		alreadyDone = false,
-		countingRuns = 0,
-		maxRuns = 15;
+		// Variables
+		var fields = [ "Central ID", "Issue Tracker ID", "Site's URL", "WordPress ID", "Sandbox URL", "url", "braintree_url" ];
+		var field = "",
+			url = "",
+			alreadyDone = false,
+			countingRuns = 0,
+			maxRuns = 15;
 
-	// Script to run
-	function clickableScript() {
+		// Script to run
+		function clickableScript() {
 
-		// Get the rows is an object
-		var rows = document.getElementsByClassName( 'gwt-TextBox' );
+			// Get the rows is an object
+			var rows = document.getElementsByClassName( 'gwt-TextBox' );
 
-		// Walk the object
-		for( var i = 0; i < rows.length; i++ ) {
-			var exists = false;
+			// Walk the object
+			for( var i = 0; i < rows.length; i++ ) {
+				var exists = false;
 
-			// Get the HTML from the row
-			field = rows[ i ].name;
+				// Get the HTML from the row
+				field = rows[ i ].name;
 
-			// If it's not a field we're looking for, then skip.
-			if ( fields.indexOf( field ) < 0 ) {
-				if ( log ) console.log( 'Skipped. Field not in array: ' + field );
-				countingRuns++;
-				if ( log ) console.log( 'Script ran ' + countingRuns + ' times so far. (Non-matching field.)' );
-				continue;
-			}
-
-			// Create the id for the field
-			var id = field.toLowerCase().replace( ' ', '-' ).replace( "'", '' );
-			if ( log ) console.log( 'ID: ' + id );
-
-			// Check if the ID exists. If it exists it means we already created it, so skip.
-			if ( null != document.getElementById( id ) ) {
-				if ( log ) console.log( 'Skipped at ID: ' + id );
-				exists = true;
-			}
-
-			// If the icon doesn't exist yet, then create.
-			if ( false === exists ) {
-				if ( log ) console.log( 'Creating icon' );
-
-				// Now we can start creating...
-
-				// Get the value of the field
-				var val = rows[ i ].value;
-				if ( log ) console.log( field + ': ' + val );
-
-				// If they are URLs, then use the value
-				if ( val.search( 'http' ) >= 0 ) {
-					if ( log ) console.log( "Found 'http', using value as URL. . " + val );
-					url = val;
-				} else if ( field == "Site's URL" ) {
-					if ( log ) console.log( "Found Site's URL - " + val );
-					url = 'https://' + val;
-				} else if ( field == "WordPress ID" ) {
-					if ( log ) console.log( "Found WordPress ID - " + val );
-					url = 'https://theeventscalendar.com/wp-admin/user-edit.php?user_id=' + val;
-				} else if ( field == "Issue Tracker ID" || field == "Central ID" ) {
-					if ( log ) console.log( "Found Issue Tracker ID - " + val );
-					/* If it doesn't contain a dash, then it's Central */
-					if ( val.search( '-' ) < 0 ) {
-						if ( log ) console.log( "Found Central - " + val );
-						url = 'https://central.tri.be/issues/';
-					}
-					/* Otherwise it is Jira */
-					else {
-						if ( log ) console.log( "Found Jira - " + val );
-						url = 'https://theeventscalendar.atlassian.net/browse/';
-					}
-					url = url + val;
+				// If it's not a field we're looking for, then skip.
+				if ( fields.indexOf( field ) < 0 ) {
+					if ( log ) console.log( 'Skipped. Field not in array: ' + field );
+					countingRuns++;
+					if ( log ) console.log( 'Script ran ' + countingRuns + ' times so far. (Non-matching field.)' );
+					continue;
 				}
 
-				// Creating the container.
-				var linkContainer = document.createElement( 'span' );
-				linkContainer.id = id;
-				linkContainer.innerHTML = '<a href="' + url + '" target="_blank" title="Open link in new window">👁️</a>';
-				linkContainer.style = 'position: absolute; right: 10px; z-index: 9;';
-				rows[ i ].parentNode.insertBefore( linkContainer, rows[ i ] );
+				// Create the id for the field
+				var id = field.toLowerCase().replace( ' ', '-' ).replace( "'", '' );
+				if ( log ) console.log( 'ID: ' + id );
 
-				alreadyDone = true;
-				if ( log ) console.log( "Script ran. Setting variable true." );
+				// Check if the ID exists. If it exists it means we already created it, so skip.
+				if ( null != document.getElementById( id ) ) {
+					if ( log ) console.log( 'Skipped at ID: ' + id );
+					exists = true;
+				}
 
-				countingRuns++;
-				if ( log ) console.log( "Script ran " + countingRuns + " times so far." );
+				// If the icon doesn't exist yet, then create.
+				if ( false === exists ) {
+					if ( log ) console.log( 'Creating icon' );
 
-			} // for ( var i=0; i<rows.length; i++ )
+					// Now we can start creating...
 
+					// Get the value of the field
+					var val = rows[ i ].value;
+					if ( log ) console.log( field + ': ' + val );
+
+					// If they are URLs, then use the value
+					if ( val.search( 'http' ) >= 0 ) {
+						if ( log ) console.log( "Found 'http', using value as URL. . " + val );
+						url = val;
+					} else if ( field == "Site's URL" ) {
+						if ( log ) console.log( "Found Site's URL - " + val );
+						url = 'https://' + val;
+					} else if ( field == "WordPress ID" ) {
+						if ( log ) console.log( "Found WordPress ID - " + val );
+						url = 'https://theeventscalendar.com/wp-admin/user-edit.php?user_id=' + val;
+					} else if ( field == "Issue Tracker ID" || field == "Central ID" ) {
+						if ( log ) console.log( "Found Issue Tracker ID - " + val );
+						/* If it doesn't contain a dash, then it's Central */
+						if ( val.search( '-' ) < 0 ) {
+							if ( log ) console.log( "Found Central - " + val );
+							url = 'https://central.tri.be/issues/';
+						}
+						/* Otherwise it is Jira */
+						else {
+							if ( log ) console.log( "Found Jira - " + val );
+							url = 'https://theeventscalendar.atlassian.net/browse/';
+						}
+						url = url + val;
+					}
+
+					// Creating the container.
+					var linkContainer = document.createElement( 'span' );
+					linkContainer.id = id;
+					linkContainer.innerHTML = '<a href="' + url + '" target="_blank" title="Open link in new window">👁️</a>';
+					linkContainer.style = 'position: absolute; right: 10px; z-index: 9;';
+					rows[ i ].parentNode.insertBefore( linkContainer, rows[ i ] );
+
+					alreadyDone = true;
+					if ( log ) console.log( "Script ran. Setting variable true." );
+
+					countingRuns++;
+					if ( log ) console.log( "Script ran " + countingRuns + " times so far." );
+
+				} // for ( var i=0; i<rows.length; i++ )
+			}
 			// If script already ran and found results, then stop.
 			if ( alreadyDone ) {
 				console.log( "Clickafy script ran with success. Stopping script." );
