ba96897d99e3a24809dd15795866cdccc62bc01b
András Guseo <andras@theeventscalendar.com>
2021-06-28 18:47:06 +0200

    Added Erica, Rob, and Phil

--- dotorg/dotorg-hider.user.js
+++ dotorg/dotorg-hider.user.js
@@ -42,7 +42,8 @@
         'courane01',                    // Courtney Robertson           - 2017-02-22
         'deblynprado',                  // Deblyn Prado                 - 2019-04-11
         'djbramer',                     // Dan Bramer
-        'erishel',                      // Edward Rishel                - 2018-03-12 to 2018-12-31	
+        'eeide',                        // Erica Eide                   - 2021-05-24
+        'erishel',                      // Edward Rishel                - 2018-03-12 to 2018-12-31
         'eugenekyale',                  // Eugene Kyale                 - 2020-09-01
         'eugenetribe',                  // Eugene Kyale                 - 2020-09-01
         'geoffbel',                     // Geoffroy 'LeGeoff' Belanger  - 2016-01-20
@@ -65,17 +66,19 @@
         'nikrosales',                   // Nik Rosales                  - 2020-02-24
         'patriciahillebrandt',          // Patricia Hillebrandt         - 2017-06-09
         'rafsuntaskin',                 // Rafsun Chowdhury             - 2020-03-30
+        'robelemental',                 // Rob Liy                      - 2021-05-24
         'sdenike',                      // Shelby DeNike                - 2018-10-08 to 2019-08-31
         'shatterdorn1',                 // Truman Dorn                  - 2021-03-01
         'sjaure',                       // Santiago Jaureguiberry       - 2019-05-26
         'skyshab',                      // Jason 'Sky' Shabatura        - 2018-01-02
+        'tecphil',                      // Phil Hodges (Community Mgr)  - 2021-04
         'theeventscalendar',            // The Events Calendar          - 2020-12-18
         'tokyobiyori',                  // Ali Darwich                  - 2018-11-19
         'tribalmike',                   // Mike Cotton                  - 2018-10-11
         'tribecari',                    // Caroline                     - 2016-05-16 to 2017-12-31
         'translationsbymoderntribe',    // Modern Tribe Translations    - 2020-03-30
         'vicskf',                       // Victor Zarranz               - 2017-xx-xx
-        'zbtirrell',                    // Zach Tirrell                 - 
+        'zbtirrell',                    // Zach Tirrell                 -
     ];
 
     var i, j;
