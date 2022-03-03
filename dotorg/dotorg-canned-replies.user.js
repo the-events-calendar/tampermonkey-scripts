e15b4de206ba06128f7240bf6a333be2a5097f5b
Gustavo Bordoni <bordoni.dev@gmail.com>
2022-03-03 01:11:22 -0500

    Update dotorg-canned-replies.user.js

--- dotorg/dotorg-canned-replies.user.js
+++ dotorg/dotorg-canned-replies.user.js
@@ -70,7 +70,7 @@
 
     obj.processVariables = ( reply ) => {
         const context = {
-            'name': $( '.bbp-lead-topic .bbp-user-nicename' ).text().replace( '(', '' ).replace( '}', '' ),
+            'name': $( '.bbp-lead-topic .bbp-user-nicename' ).text().replace( '(', '' ).replace( ')', '' ),
             'plugin_url': $( '.entry-meta.sidebar .plugin-meta-icon' ).next( 'li' ).find( 'a' ).attr( 'href' ),
         };
 
