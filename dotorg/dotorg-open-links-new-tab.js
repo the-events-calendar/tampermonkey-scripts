d06cee560ec1678220629548718d4c02594dd2c2
James Welbes <james.welbes@gmail.com>
2022-03-14 10:30:53 -0500

    add script that opens links in new tab

---
+++ dotorg/dotorg-open-links-new-tab.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Opens all links in new tab
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  This adds target="_blank" to all <a> tags
+// @author       You
+// @match        https://wordpress.org/support/plugin/the-events-calendar*
+// @include      https://wordpress.org/support/topic*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=wordpress.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const links = document.querySelectorAll("a");
+
+    links.forEach( (link)=> {
+        link.target="_blank";
+    })
+
+})();
