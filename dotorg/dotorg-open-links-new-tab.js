1728eb254f73162b317a8f50116588d57a1a42ce
James Welbes <james.welbes@gmail.com>
2022-03-14 16:24:04 -0500

    update author, download/update url. add space to end of file

--- dotorg/dotorg-open-links-new-tab.js
+++ dotorg/dotorg-open-links-new-tab.js
@@ -3,11 +3,13 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  This adds target="_blank" to all <a> tags
-// @author       You
-// @include        https://wordpress.org/support/plugin/the-events-calendar*
+// @author       James Welbes
+// @include      https://wordpress.org/support/plugin/the-events-calendar*
 // @include      https://wordpress.org/support/topic*
 // @include      https://wordpress.org/support/plugin/event-tickets*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wordpress.org
+// @downloadURL  https://github.com/the-events-calendar/tampermonkey-scripts/raw/main/dotorg/dotorg-open-links-new-tab.js
+// @updateURL    https://github.com/the-events-calendar/tampermonkey-scripts/raw/main/dotorg/dotorg-open-links-new-tab.js
 // @grant        none
 // ==/UserScript==
 
