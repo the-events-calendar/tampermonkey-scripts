c82a0c577e68c848deeed4232257a6315ad75a3a
András Guseo <andras@theeventscalendar.com>
2023-01-22 23:27:17 +0100

    Add new version numbers (248-251)

--- zendesk/zendesk-plugin-versions.user.js
+++ zendesk/zendesk-plugin-versions.user.js
@@ -245,6 +245,10 @@
             151: { name: "G22.parksosaurus",  date: "Nov 9",      tec: "6.0.3.1",   pro: "6.0.2.1",   vev: "1.13.2",   fib: "5.4.0",    ebt: "4.6.12",  eti: "5.5.4x",     etp: "5.6.3x",    cev: "4.10.2x",   ctx: "4.8.2x",   apm: "4.5.2",  iwp: "1.0.3",  woo: "7.1.0", edd: "3.1.0.2" },
             152: { name: "B22.tiramisu",      date: "Nov 15",     tec: "6.0.4x",    pro: "6.0.3x",    vev: "1.13.2",   fib: "5.4.0",    ebt: "4.6.12",  eti: "5.5.4",      etp: "5.6.3",     cev: "4.10.2",    ctx: "4.8.2",    apm: "4.5.2",  iwp: "1.0.3",  woo: "7.1.0", edd: "3.1.0.2" },
             153: { name: "B22.wagashi",       date: "Nov 29",     tec: "6.0.5x",    pro: "6.0.4x",    vev: "1.13.3x",  fib: "5.4.0",    ebt: "4.6.12",  eti: "5.5.4",      etp: "5.6.3",     cev: "4.10.2",    ctx: "4.8.2",    apm: "4.5.2",  iwp: "1.0.3",  woo: "7.1.0", edd: "3.1.0.3" },
+            154: { name: "G22.tyrannosaurus", date: "Dec 8",      tec: "6.0.5",     pro: "6.0.4",     vev: "1.13.3",   fib: "5.4.0",    ebt: "4.6.12",  eti: "5.5.5x",     etp: "5.6.4x",    cev: "4.10.3x",   ctx: "4.8.3x",   apm: "4.5.2",  iwp: "1.0.3",  woo: "7.1.1", edd: "3.1.0.3" },
+            155: { name: "B22.zeppole",       date: "Dec 14",     tec: "6.0.6.2x",  pro: "6.0.5.1x",  vev: "1.13.3",   fib: "5.4.1x",   ebt: "4.6.12",  eti: "5.5.5",      etp: "5.6.4",     cev: "4.10.3",    ctx: "4.8.3",    apm: "4.5.2",  iwp: "1.0.3",  woo: "7.2.0", edd: "3.1.0.3" },
+            156: { name: "G23.artichoke",     date: "Jan 16",     tec: "6.0.6",     pro: "6.0.5",     vev: "1.13.3",   fib: "5.4.1",    ebt: "4.6.12",  eti: "5.5.6x",     etp: "5.6.5x",    cev: "4.10.3",    ctx: "4.8.3",    apm: "4.5.2",  iwp: "1.0.3",  woo: "7.3.0", edd: "3.1.0.4" },
+            157: { name: "B23.alexa",         date: "Jan 18",     tec: "6.0.7.1x",  pro: "6.0.6x",    vev: "1.13.4x",  fib: "5.4.2x",   ebt: "4.6.12",  eti: "5.5.6",      etp: "5.6.5",     cev: "4.10.3",    ctx: "4.8.3",    apm: "4.5.2",  iwp: "1.0.3",  woo: "7.3.0", edd: "3.1.0.4" },
         };
 
         // The number of releases (the length of the object)
